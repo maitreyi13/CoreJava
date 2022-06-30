ee36377c7e73be4bf7de1423590c0f19885a765c
Maitreyi Kulkarni <107098811+maitreyi13@users.noreply.github.com>
2022-06-30 23:09:57 +0530

    Find the Largest Among Three Numbers

--- LargestNum.java
+++ LargestNum.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class largestNum {
+public class LargestNum {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter three numbers:");
